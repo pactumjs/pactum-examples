c388470bf78f143776eab0c3c85f6d7912deee6d
Anudeep <asa.anudeep4@gmail.com>
2022-06-02 19:31:55 +0530

    Update HealthController.java

--- code-coverage-spring-java/src/main/java/com/example/demo/controllers/HealthController.java
+++ code-coverage-spring-java/src/main/java/com/example/demo/controllers/HealthController.java
@@ -1,5 +1,20 @@
-package com.examples.demo.controllers;
+package com.example.demo.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.models.Health;
+
+@RestController
+@RequestMapping("/api/v1")
 public class HealthController {
+
+  @GetMapping("/health")
+  public Health get() {
+    Health health = new Health();
+    health.message = "OK";
+    return health;
+  }
   
 }
