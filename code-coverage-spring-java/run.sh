c63ad8607fc4259680c32eca0ffb2a342b4ec0eb
Anudeep <asa.anudeep4@gmail.com>
2022-06-02 20:15:46 +0530

    Update run.sh

--- code-coverage-spring-java/run.sh
+++ code-coverage-spring-java/run.sh
@@ -8,3 +8,9 @@ sleep 10s
 
 echo "run tests"
 npm run test
+
+echo "dump coverage"
+docker-compose exec -T app java -jar jacococli.jar dump --address localhost --port 33933 --destfile coverage/jacoco.exec
+
+echo "parse coverage"
+docker-compose exec -T app java -jar jacococli.jar report coverage/jacoco.exec --classfiles target/classes/com --sourcefiles src/main/java/ --html coverage/html --xml coverage/coverage.xml
